05c1c623d906c65a6cbfe1b15cd481b544de93d0
wstab <willstabile@gmail.com>
2025-09-27 15:33:28 -0400

    Update index.py

--- src/index.py
+++ src/index.py
@@ -1,1 +1,9 @@
+from workers import Response, WorkerEntrypoint
+class Default(WorkerEntrypoint):
+    async def fetch(self, request):
+        # Do anything else you'd like on request here!
 
+        # Query D1 - we'll list all tables in our database in this example
+        results = await self.env.DB.prepare("PRAGMA table_list").run()
+        # Return a JSON response
+        return Response.json(results)
