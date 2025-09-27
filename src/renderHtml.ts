93e52980c83bd83c00350e5a2cba9c05882d0ee3
wstab <willstabile@gmail.com>
2025-09-27 14:59:00 -0400

    Update renderHtml.ts

--- src/renderHtml.ts
+++ src/renderHtml.ts
@@ -18,7 +18,7 @@ export function renderHtml(content: string) {
         </header>
         <main>
           <p>Your D1 Database contains the following data:</p>
-          <pre><code><span style="color: #0E838F">&gt; </span>SELECT * FROM comments LIMIT 3;<br>${content}</code></pre>
+          <pre><code><span style="color: #0E838F">&gt; </span>SELECT * FROM jobs LIMIT 3;<br>${content}</code></pre>
           <small class="blue">
             <a target="_blank" href="https://developers.cloudflare.com/d1/tutorials/build-a-comments-api/">Build a comments API with Workers and D1</a>
           </small>
